ffc55aa178e9fc3b24c8f61a540a55018567ba43
Emily <erpersons@gmail.com>
2017-08-31 16:35:37 -0500

    done with rainbow

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
-console.log("Emily");
-console.log("changes");
+var lunchboxes = []
+
+
+// constructor
+function Lunchbox ( sizeIn, handleIn, colorIn, imageIn){ //add In to avoid confusion with other properties
+    //is allowed to begin with capital L because constructor
+    this.size = sizeIn
+    this.handle = handleIn
+    this.color = colorIn
+    this.image = imageIn
+} //end Lunchbox constructor
+
+//create lunchbxes with constructor & push into our array
+lunchboxes.push( new Lunchbox( "medium", true, "black", true));
+lunchboxes.push( new Lunchbox( "medium", true, "blue faux snakeskin", false));
+lunchboxes.push(new Lunchbox("large", true, "blue", true));
+
+
+//console.log(lunchboxes);
+
+//search for lunchboxes with images
+function findLunchboxImages( images ){
+    //create empty array to hold results
+    var results = [];
+    for( var i = 0; i<lunchboxes.length; i++ ) {
+        //for each lunchbox
+        //are we looking for images?
+        if( images ) {
+            if( lunchboxes [ i ].image ) {
+                results.push( lunchboxes[ i ] );
+            } // found match
+        } // end image
+        else {
+            if( !lunchboxes[ i ] .image){
+                results.push( lunchboxes [ i ] );
+            } // found match
+        } //end no image
+    }// end for
+    return results
+} // end findLunchboxImages
