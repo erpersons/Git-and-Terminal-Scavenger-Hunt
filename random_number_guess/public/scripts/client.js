b1f81d409826f0686f9e609ed36fff2463bf44b2
Emily <erpersons@gmail.com>
2017-09-08 14:28:24 -0500

    game logic completed

--- random_number_guess/public/scripts/client.js
+++ random_number_guess/public/scripts/client.js
@@ -2,17 +2,22 @@ var winningNumb = 0;
 
 $(document).ready(onReady);
 
-function onReady(){
+function onReady() {
     $('#startButton').on('click', getRandomNumb);
+    $('#playButton').on('click', compareGuess);
 }
 
-function getRandomNumb() {
+function getRandomNumb(event) {
+    event.preventDefault();
+
     var levelVal = $('#difficultySetting input[name=level]:checked').val();
-    
+
     $.ajax({
         type: 'POST',
         url: '/maxNumber',
-        data: {max: levelVal},
+        data: {
+            max: levelVal
+        },
         success: function (res) {
             console.log(res);
             console.log('checked: ', levelVal);
@@ -21,3 +26,20 @@ function getRandomNumb() {
         }
     });
 }
+
+function compareGuess(event) {
+    event.preventDefault();
+    var guessArray = $('#playerData input[name=guess]');
+    console.log('player guesses: ', guessArray);
+    for (var i = 0; i < guessArray.length; i++){
+        var guess = parseInt($(guessArray[i]).val());
+        console.log('guess ' + [i] + ': ' + guess);
+        if (guess === winningNumb) {
+            console.log('You win!');
+        } else if (guess < winningNumb) {
+            console.log('Too low...');
+        } else {
+            console.log('Too high...');
+        }
+    }
+} // end compareGuess
--- random_number_guess/server.js
+++ random_number_guess/server.js
@@ -5,37 +5,41 @@ var port = 5000;
 var bodyparser = require('body-parser');
 
 function randomNumberGen(max) {
-    var randomNumb = Math.floor(Math.random()*max);
+    var randomNumb = Math.floor(Math.random() * max);
     return randomNumb;
 }
 
- // middleware
+// middleware
 app.use(express.static('public'));
-app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
 
-app.listen(port, function() {
-    console.log( 'listening on port', port);
+app.listen(port, function () {
+    console.log('listening on port', port);
 })
 
 //base url
-app.get('/', function( req, res ){
+app.get('/', function (req, res) {
 
     res.sendFile(path.join(__dirname, './public/views/index.html'));
     console.log('inside app.get');
 })
 
-app.get('/game', function(req, res) {
+app.get('/game', function (req, res) {
     console.log('received request');
     var numbObj = {
         gameNumber: randomNumberGen(100)
-        };
+    };
     res.send(numbObj);
 });
 
-app.post('/maxNumber', function(req, res) {
+app.post('/maxNumber', function (req, res) {
     var maxNumbObj = req.body;
     var maximus = maxNumbObj.max;
     var maxPrime = randomNumberGen(maximus);
     console.log("maxPrime -->", maxPrime);
-    res.send({returnedNumb: maxPrime});
+    res.send({
+        returnedNumb: maxPrime
+    });
 });
