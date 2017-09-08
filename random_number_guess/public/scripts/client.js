27fd6c21e4f6eb33e12051717aa702694a71b4d1
Emily <erpersons@gmail.com>
2017-09-08 16:28:52 -0500

    comments added

--- random_number_guess/public/scripts/client.js
+++ random_number_guess/public/scripts/client.js
@@ -1,15 +1,22 @@
 var winningNumb = 0;
+var guessCounter = 0;
+var guessLimit = 3;
 
 $(document).ready(onReady);
 
 function onReady() {
-    $('#startButton').on('click', getRandomNumb);
+    $('#gameArea').hide();
+    $('#startButton').on('click', startGame);
     $('#playButton').on('click', compareGuess);
-}
+    $('#rageQuit').on('click', rageQuit);
+} //end onReady
 
-function getRandomNumb(event) {
+function startGame(event) {
     event.preventDefault();
-
+    resetGameArea();
+    $('#gameArea').show();
+    $('#currentGuesses').text(guessCounter);
+    $('#guessLimit').text(guessLimit);
     var levelVal = $('#difficultySetting input[name=level]:checked').val();
 
     $.ajax({
@@ -29,17 +36,53 @@ function getRandomNumb(event) {
 
 function compareGuess(event) {
     event.preventDefault();
-    var guessArray = $('#playerData input[name=guess]');
-    console.log('player guesses: ', guessArray);
-    for (var i = 0; i < guessArray.length; i++){
-        var guess = parseInt($(guessArray[i]).val());
-        console.log('guess ' + [i] + ': ' + guess);
-        if (guess === winningNumb) {
-            console.log('You win!');
-        } else if (guess < winningNumb) {
-            console.log('Too low...');
-        } else {
-            console.log('Too high...');
+    guessCounter++;
+    console.log(" Guess -->", guessCounter);
+    $('#currentGuesses').text(guessCounter);
+    if (guessCounter < guessLimit) {
+        var guessArray = $('#playerData input[name=guess]');
+        console.log('player guesses: ', guessArray);
+        for (var i = 0; i < guessArray.length; i++) {
+            var $input = $(guessArray[i]);
+            var $p = $input.next('p');
+            var guess = parseInt($(guessArray[i]).val());
+            console.log('guess ' + [i] + ': ' + guess);
+            $input.val('');
+            if (guess === winningNumb) {
+                console.log('You win!');
+                $p.text('You guessed ' + guess + '! You win!');
+            } else if (guess < winningNumb) {
+                console.log('Too low...');
+                $p.text('You guessed ' + guess + '. Too low...');
+            } else {
+                console.log('Too high...');
+                $p.text('You guessed ' + guess + '. Too high...');
+            }
         }
     }
+    else {
+        $('#gameArea').hide();
+        alert("You all failed. You should find an easier game.");
+    }
 } // end compareGuess
+
+function resetGameArea() {
+    guessCounter = 0;
+    $('#currentGuesses').text(guessCounter);
+    $('#playerData input[name=guess]').each(function(element) {
+        console.log(element);
+        $(element).val('');
+    });
+
+
+
+
+}
+
+function rageQuit() {
+    $('#difficultySetting input[name=level]:checked').each(function(element) {
+        $(element).prop('checked','false');
+    });
+
+    $('#gameArea').hide();
+}
